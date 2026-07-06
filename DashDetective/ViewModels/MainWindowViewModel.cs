3a89c8df0a24fdaacd0679f203bcef877e6eef2c
SophiaScherer <Sophiasch@gmail.com>
2026-07-06 00:40:04 -0700

    Initial commit

---
+++ DashDetective/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,5 @@
+﻿namespace DashDetective.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase {
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ DashDetective/ViewModels/ViewModelBase.cs
@@ -0,0 +1,6 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DashDetective.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject {
+}
