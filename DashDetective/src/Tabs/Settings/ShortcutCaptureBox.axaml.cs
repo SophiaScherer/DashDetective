8b92b264afc8a98d071bb576bbebd334ec23eab8
SophiaScherer <Sophiasch@gmail.com>
2026-09-23 08:22:59 -0700

    Drop the capture box's unused shortcuts import
    - The key rule moved to CaptureKeys, so the control no longer references GestureFormatter

--- DashDetective/src/Tabs/Settings/ShortcutCaptureBox.axaml.cs
+++ DashDetective/src/Tabs/Settings/ShortcutCaptureBox.axaml.cs
@@ -2,7 +2,6 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
-using DashDetective.Shared.Shortcuts;
 using System;
 
 namespace DashDetective.Tabs.Settings;
